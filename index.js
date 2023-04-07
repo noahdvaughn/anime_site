4d784423a2877266f0040ea78bfa1ad07bb07215
Noah Vaughn <noahdvaughn@gmail.com>
2023-04-06 20:05:23 -0400

    added nodemon:

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
 const AppRouter = require('./routes/AppRouter')
-require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3001
