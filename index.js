0e17d2afe15a85ee7320e90e78d4f7e9e287b3fe
Noah Vaughn <noahdvaughn@gmail.com>
2023-04-06 20:44:28 -0400

    updated index build

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(cors())
-app.use(express.static(path.join(`${__dirname}/client/dist`)))
+// app.use(express.static(path.join(`${__dirname}/client/dist`)))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -17,8 +17,8 @@ app.get('/', (req, res) => {
   res.send('This is the base path!')
 })
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/client/dist/index.html`))
-})
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(`${__dirname}/client/dist/index.html`))
+// })
 
 app.listen(PORT, () => console.log(`Server Running On Port ${PORT} . . . `))
